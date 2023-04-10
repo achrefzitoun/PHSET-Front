c78679793e6cf802f4425de80985c30e8c3b16d5
Achref Zitoun <achref.zitoun@esprit.tn>
2023-04-10 16:02:13 +0100

    Integration Template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TemplateComponent } from './template/template.component';
+import { TestComponent } from './test/test.component';
+import { NavBarFrontComponent } from './nav-bar-front/nav-bar-front.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TemplateComponent,
+    TestComponent,
+    NavBarFrontComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/nav-bar-front/nav-bar-front.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-nav-bar-front',
+  templateUrl: './nav-bar-front.component.html',
+  styleUrls: ['./nav-bar-front.component.css']
+})
+export class NavBarFrontComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
